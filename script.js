5a85c4210bbc6d88119200b322a2af1829c21fc9
MOHAMMAD SHAAN <mdsworld2006@gmail.com>
2026-03-28 16:02:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Future: Dark/light toggle, background particles, etc.
 });
+
+const projects = [
+  {
+    name: "ISI Summer Internship",
+    start: "2026-03-01",
+    end: "2026-04-15",
+    link: "#"
+  },
+  {
+    name: "IIT Madras Winter Project",
+    start: "2026-10-01",
+    end: "2026-11-10",
+    link: "#"
+  }
+];
+
+const projects = [
+  {
+    name: "ISI Summer Internship",
+    start: "2026-03-01",
+    end: "2026-04-15",
+    link: "#"
+  },
+  {
+    name: "IIT Madras Winter Project",
+    start: "2026-10-01",
+    end: "2026-11-10",
+    link: "#"
+  }
+];
